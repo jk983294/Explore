e28a2e02b17cc57f62c00bec6fb53b01ef538d99
jk983294 <jk983294@gmail.com>
2016-01-21 23:38:00 +0800

    add template

---
+++ design_pattern/src/com/victor/design/behavioral/template/TemplatePatternDemo.java
@@ -0,0 +1,17 @@
+package com.victor.design.behavioral.template;
+
+/**
+ * an abstract class exposes defined way(s)/template(s) to execute its methods.
+ * Its subclasses can override the method implementation as per need
+ * but the invocation is to be in the same way as defined by an abstract class.
+ */
+public class TemplatePatternDemo {
+    public static void main(String[] args) {
+
+        Game game = new Cricket();
+        game.play();
+        System.out.println();
+        game = new Football();
+        game.play();
+    }
+}
