f966034c6625673dcca85c5f1cd85a3b3a4b69d3
jk983294 <jk983294@gmail.com>
2016-01-22 21:07:59 +0800

    add visitor

---
+++ design_pattern/src/com/victor/design/behavioral/visitor/Keyboard.java
@@ -0,0 +1,9 @@
+package com.victor.design.behavioral.visitor;
+
+public class Keyboard implements ComputerPart {
+
+    @Override
+    public void accept(ComputerPartVisitor computerPartVisitor) {
+        computerPartVisitor.visit(this);
+    }
+}
