64644c79f8b91c4d64a31b96b5d790b53d4d607b
jk983294 <jk983294@gmail.com>
2016-01-26 21:33:37 +0800

    pattern add web pattern

---
+++ design_pattern/src/com/victor/design/web/businessdelegate/BusinessService.java
@@ -0,0 +1,5 @@
+package com.victor.design.web.businessdelegate;
+
+public interface BusinessService {
+    public void doProcessing();
+}
---
+++ design_pattern/src/com/victor/design/web/businessdelegate/EJBService.java
@@ -0,0 +1,9 @@
+package com.victor.design.web.businessdelegate;
+
+public class EJBService implements BusinessService {
+
+    @Override
+    public void doProcessing() {
+        System.out.println("Processing task by invoking EJB Service");
+    }
+}
