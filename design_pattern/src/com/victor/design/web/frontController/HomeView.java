1f476967845e069a23f16b5ddfe0264d95c57138
jk983294 <jk983294@gmail.com>
2016-02-03 20:35:52 +0800

    add FrontController pattern

---
+++ design_pattern/src/com/victor/design/web/frontController/HomeView.java
@@ -0,0 +1,7 @@
+package com.victor.design.web.frontController;
+
+public class HomeView {
+    public void show(){
+        System.out.println("Displaying Home Page");
+    }
+}
---
+++ design_pattern/src/com/victor/design/web/frontController/StudentView.java
@@ -0,0 +1,7 @@
+package com.victor.design.web.frontController;
+
+public class StudentView {
+    public void show(){
+        System.out.println("Displaying Student Page");
+    }
+}
