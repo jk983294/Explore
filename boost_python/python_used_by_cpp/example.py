e180d49e2e6a7b04da85c4524749755cbccde72c
jk983294 <jk983294@gmail.com>
2018-07-10 22:54:25 +0800

    boost_python

---
+++ boost_python/python_used_by_cpp/example.py
@@ -0,0 +1,10 @@
+#!/bin/python
+
+
+def stringlist():
+    return ["foo", "bar", "baz"]
+
+
+if __name__ == "__main__":
+    print "This is a python script"
+    print "It's running within a C++ program."
